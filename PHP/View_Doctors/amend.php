215da1f4651c905c206773173b76834233d17adc
conorbrooke77 <68202359+conorbrooke77@users.noreply.github.com>
2022-03-16 19:17:49 +0000

    Amend Update

---
+++ PHP/View_Doctors/amend.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<?php 
+    session_start();
+    include '../db.inc.php';
+
+    $sql = "SELECT * FROM Doctor";
+
+    $result = $con->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result-> fetch_assoc()) {
+            echo "<tr><td>" . $row["doctorID"] . "</td><td>" . $row["Surname"] . "</td><td>" . $row["Firstname"] . "</td><td>" . $row["SurgeryTelephoneNumber"] . "</td><td>" . $row["SurgeryMobileNumber"] . "</td><td>" . $row["HomeTelephoneNumber"] . "</td><td>" 
+            . $row["SurgeryStreet"] . "</td><td>" . $row["SurgeryTown"] . "</td><td>" . $row["SurgeryCounty"] . "</td><td>" . $row["HomeStreet"] . "</td><td>" . $row["HomeTown"] . "</td><td>" . $row["HomeCounty"] . "</td></tr>";
+        }
+    } else {
+        echo "No Result"
+    }
+
+    if (!$result = mysqli_query($con,$sql))
+        {
+	        die('error in querying the database' . mysqli_error($con));
+	
+        }
+?>
