da5d5fd0a0118a7c5a8db077addf25159ecd9ca1
conorbrooke77 <68202359+conorbrooke77@users.noreply.github.com>
2022-03-16 17:40:43 +0000

    Insert Update

---
+++ PHP/Insert_Doctor/insert_doctor.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<?php
+    include 'db.inc.php'; //Datbase Connection
+
+    echo "The information below has been successfully recieved. <br>";
+
+    //Query to grab doctorID
+    $data = "SELECT * FROM Doctor ORDER BY doctorID DESC LIMIT 1";
+    
+    //Make query
+    $result = mysqli_query($con,$data);
+
+    //Fetch data from result
+
+    while ($row = mysqli_fetch_array($result)) {
+        $doctorID = $row['doctorID'];
+    }
+
+    // Incrementing DoctorID
+    $doctorID = $doctorID+1;
+
+    if (!mysqli_query($con,$data))
+    {
+        die ("an error in the sql query: " . mysqli_error($con) );
+    }
+
+    $sql = "insert into Doctor (doctorID, Surname, Firstname, SurgeryTelephoneNumber, SurgeryMobileNumber, HomeTelephoneNumber, SurgeryStreet, SurgeryTown, SurgeryCounty, HomeStreet, HomeTown, HomeCounty)
+    VALUES ('$doctorID','$_POST[surname]','$_POST[firstname]','$_POST[telephoneNumber]','$_POST[mobileNumber]','$_POST[homeTelephone]','$_POST[surgeryStreet]',
+            '$_POST[surgeryTown]','$_POST[surgeryCounty]','$_POST[homeStreet]','$_POST[homeTown]','$_POST[homeCounty]')";
+
+    if (!mysqli_query($con,$sql))
+        {
+	        die ("an error in the sql query: " . mysqli_error($con) );
+        }
+    echo "<br>A record has been added for Doctor " . $_POST['firstname'] . " " . $_POST['surname'] . " with a unique ID of " . $doctorID;
+
+    mysqli_close($con);
+
+?>
+	<form action = "../../File_Maintenance/Add_Doctor.html" method = "POST">
+	    <br>
+	    <input type = "submit" value = "return to insert page"/>
+	</form>
